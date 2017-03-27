6e15c131e8168cf344ab2289b673d67d81f754fc
v.adhithyan <aavispeaks@gmail.com>
2017-03-27 20:55:20 +0530

    initial commit

---
+++ mobile/src/main/java/ceg/avtechlabs/mba/activities/MainActivity.kt
@@ -0,0 +1,14 @@
+package ceg.avtechlabs.mba.activities
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+import ceg.avtechlabs.mba.R
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
